34e8105d9233c14a4ad76d93e93934f5612a13fc
paulo <souzafontanelapaulo@gmail.com>
2025-03-28 20:21:38 -0300

    first commit

---
+++ app/src/main/java/com/example/cacatesouro/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.cacatesouro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.cacatesouro.ui.theme.CacaTesouroTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+           val navController = rememberNavController()
+            NavHost(
+                navController = navController,
+                startDestination = "/home"
+            ){
+                composable("/home"){
+                    Tela("home")
+                }
+                composable("/P1"){
+                    Tela("P1")
+                }
+                composable("/P2"){
+                    Tela("P2")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Tela(nomeTela: String){
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = " $nomeTela"
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CacaTesouroTheme {
+        Greeting("Android")
+    }
+}
